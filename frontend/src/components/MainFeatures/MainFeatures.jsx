6c77f673aa4e3498ed7106af5e9dfdcea22f8349
marci <marcianszlovak@gmail.com>
2020-06-20 17:03:08 +0200

    feat: create separate folder for components and MainFeatures component

---
+++ frontend/src/components/MainFeatures/MainFeatures.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainFeatures.style.scss';
+
+const MainFeatures = () => {
+  return (
+    <>
+      <div className="main-features"></div>
+    </>
+  );
+};
+
+export default MainFeatures;
