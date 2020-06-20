270a31ec053326478b13e8c79edeb3c6dbec4768
marci <marcianszlovak@gmail.com>
2020-06-20 16:44:58 +0200

    feat: add separate view for the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <>I am the landing page!</>;
+};
+
+export default LandingPage;
