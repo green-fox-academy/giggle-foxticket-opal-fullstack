5ed55c4174f51dcc33eac0112cbba33d79686336
marci <marcianszlovak@gmail.com>
2020-06-21 22:54:24 +0200

    feat: add sass variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Button>HELLO</Button>
     </div>
   );
 }
