2b73be51d5a761124d2a9b4ad6c8f399412985cc
marci <marcianszlovak@gmail.com>
2020-06-22 21:51:15 +0200

    feat: add 2 more snapshot tests to button component

--- frontend/src/components/Button/Button.test.jsx
+++ frontend/src/components/Button/Button.test.jsx
@@ -6,3 +6,40 @@ it('expect to render Button component', () => {
   expect(shallow(<Button />)).toMatchSnapshot();
 });
 
+it('should render with valid props provided', () => {
+  const mockChildren = 'Click me';
+  const mockClick = console.log('I got clicked!');
+  const mockStyles = ['btn--primary--solid', 'btn--warning--solid'];
+  const mockSizes = ['btn--medium', 'btn--large'];
+  const mockTypes = ['button', 'submit'];
+  expect(
+    shallow(
+      <Button
+        className={`btn ${mockStyles} ${mockSizes}`}
+        onClick={mockClick}
+        type={mockTypes}
+      >
+        {mockChildren}
+      </Button>
+    )
+  ).toMatchSnapshot();
+});
+
+it('should not throw an error with invalid style or size props', () => {
+  const mockChildren = 'Click me';
+  const mockClick = console.log('I got clicked!');
+  const mockStyles = ['I am not valid', 'Value should be reverted to default'];
+  const mockSizes = ['I am not valid', 'Value should be reverted to default'];
+  const mockTypes = ['button', 'submit'];
+  expect(
+    shallow(
+      <Button
+        className={`btn ${mockStyles} ${mockSizes}`}
+        onClick={mockClick}
+        type={mockTypes}
+      >
+        {mockChildren}
+      </Button>
+    )
+  ).toMatchSnapshot();
+});
