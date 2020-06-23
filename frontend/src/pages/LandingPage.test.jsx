c3457950a404363e5caafd8e374b326cf5a1c0d6
marci <marcianszlovak@gmail.com>
2020-06-23 22:22:40 +0200

    fix: typo on landing page component test file

--- frontend/src/pages/LandingPage.test.jsx
+++ frontend/src/pages/LandingPage.test.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import LandingPage from './LandingPage';
 
-it('expect to render Button component', () => {
+it('expect to render Landing Page component', () => {
   expect(shallow(<LandingPage />).length).toEqual(1);
 });
