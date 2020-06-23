8e222c5f8a68b4416499c70eb7ad1f0a0d20bedd
marci <marcianszlovak@gmail.com>
2020-06-23 23:06:39 +0200

    feat: use sinon spy to test click events

--- frontend/src/components/Button/Button.test.jsx
+++ frontend/src/components/Button/Button.test.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import Button from './Button';
 
-it('expect to render Button component', () => {
-  expect(shallow(<Button />).length).toEqual(1);
-});
+describe('Test Button component', () => {
+  it('should render Button component', () => {
+    expect(shallow(<Button />).length).toEqual(1);
+    expect(shallow(<Button />)).toMatchSnapshot();
+  });
 
-it('should render the Button component', () => {
-  expect(shallow(<Button />)).toMatchSnapshot();
+  it('simulates click events', () => {
+    const mockCallBack = sinon.spy();
+    const mockChildren = 'Click me';
+
+    const button = shallow(
+      <Button
+        type="button"
+        buttonSize="btn--large"
+        buttonStyle="btn--warning--outline"
+        onClick={mockCallBack}
+      >
+        {mockChildren}
+      </Button>
+    );
+
+    button.find('button').simulate('click');
+    expect(mockCallBack).toHaveProperty('callCount', 1);
+  });
 });
