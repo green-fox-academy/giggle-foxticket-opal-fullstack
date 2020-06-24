43d57301d70fd6cd1c55578ccef0bffd1c13f280
marci <marcianszlovak@gmail.com>
2020-06-24 20:44:50 +0200

    feat: add snapshot test for Landing Page component

--- frontend/src/pages/LandingPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import LandingPage from './LandingPage';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<LandingPage />);
-  const linkElement = getByText('I am the landing page!');
-  expect(linkElement).toBeInTheDocument();
-});
