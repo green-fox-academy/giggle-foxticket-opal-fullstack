bc7d45d4a52e32e0a74bcd1648b267b49f358ddd
marci <marcianszlovak@gmail.com>
2020-06-24 19:41:07 +0200

    feat: rewrite button snapshot test

--- frontend/src/components/Button/Button.test.jsx
+++ frontend/src/components/Button/Button.test.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import Button from './Button';
 
 describe('Test Button component', () => {
   it('should render Button component', () => {
+    const tree = renderer.create(<Button />).toJSON();
+    expect(tree).toMatchSnapshot();
     expect(shallow(<Button />).length).toEqual(1);
-    expect(shallow(<Button />)).toMatchSnapshot();
   });
 
   it('simulates click events', () => {
