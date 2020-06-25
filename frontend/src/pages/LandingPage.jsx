d51226237bfdd4b99d83791ee2c3edb92bd8fa09
marci <marcianszlovak@gmail.com>
2020-06-25 17:00:21 +0200

    fix: resolve conflicts

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import MainFeatures from '../components/MainFeatures/MainFeatures';
 
 const LandingPage = () => {
-<<<<<<< HEAD
-  return <></>;
-=======
   return (
     <>
       I am the landing page!
       <MainFeatures />
     </>
   );
->>>>>>> master
 };
 
 export default LandingPage;
