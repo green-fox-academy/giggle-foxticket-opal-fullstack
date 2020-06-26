e63b3d932e0974f975b7d63d3b92fb698c350212
marci <marcianszlovak@gmail.com>
2020-06-26 22:39:58 +0200

    feat: add async function to check mysql connection before query

--- backend/src/data/connection.js
+++ backend/src/data/connection.js
@@ -2,13 +2,32 @@ import mysql from 'mysql';
 
 import config from '../config';
 
-var pool = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit: 2,
   host: config.mysql.host,
   user: config.mysql.user,
   password: config.mysql.password,
   database: config.mysql.database,
 });
+
+async function getDbStatus() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query('SELECT 1 as val');
+    console.log(rows);
+    const res = await conn.query('INSERT INTO myTable value (?, ?)', [
+      1,
+      'giggle-opal',
+    ]);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+}
 
 export const db = {
   query(query, values) {
