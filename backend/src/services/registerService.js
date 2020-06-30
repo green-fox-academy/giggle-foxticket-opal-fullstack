f8c3e042b5c7238f5adfb0aebc7d852cbeb7f21c
marci <marcianszlovak@gmail.com>
2020-06-30 19:44:25 +0200

    feat: add error handling to register service

--- backend/src/services/registerService.js
+++ backend/src/services/registerService.js
@@ -3,11 +3,16 @@ import { db } from '../data/connection';
 
 export const registerService = {
   async registerUser(user) {
-    const hashedPassword = await bcrypt.hash('123123123', 10);
+    const hashedPassword = await bcrypt.hash(user.password, 10);
 
-    await db.query(
-      `INSERT INTO foxticket.Users (username, email, password) VALUES (?, ?, ?)`,
-      [user.username, user.email, hashedPassword]
-    );
+    await db
+      .query(
+        `INSERT INTO foxticket.Users (username, email, password) VALUES (?, ?, ?)`,
+        [user.username, user.email, hashedPassword]
+      )
+      .catch(err => {
+        console.log(err.sqlMessage);
+        console.log(`Username: ${user.username}, E-mail: ${user.email}`);
+      });
   },
 };
