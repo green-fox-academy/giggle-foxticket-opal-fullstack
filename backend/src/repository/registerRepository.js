5b035f887b03cf466af0e532472b3078b6d1c926
marci <marcianszlovak@gmail.com>
2020-07-01 20:36:20 +0200

    fix: re-format repository file

--- backend/src/repository/registerRepository.js
+++ backend/src/repository/registerRepository.js
@@ -3,14 +3,18 @@ import { db } from '../data/connection';
 export const registerRepository = {
   async saveUser(user) {
     await db.query(
-      `INSERT INTO foxticket.Users (name, email, password) VALUES (?, ?, ?)`,
+      `INSERT INTO foxticket.Users (name, email, password)
+         VALUES (?, ?, ?)`,
       [user.name, user.email, user.password]
     );
   },
 
   async getUserData(username, userEmail) {
     return await db.query(
-      `SELECT name, email FROM foxticket.Users WHERE name = ? OR email = ?`,
+      `SELECT name, email
+         FROM foxticket.Users
+         WHERE name = ?
+            OR email = ?`,
       [username, userEmail]
     );
   },
