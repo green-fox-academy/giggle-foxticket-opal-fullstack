03fdc907e28fada0bb57d63707eb0902c0854d2d
marcianszlovak <marcianszlovak@gmail.com>
2020-07-04 18:10:15 +0200

    feat(register-form): add styles, extra input for password confirmation

--- frontend/src/components/RegisterForm/RegisterForm.jsx
+++ frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import useCustomForm from '../../hooks/useCustomForm';
 import './RegisterForm.styles.sass';
+import Button from '../Button/Button';
 
 const initialValues = {
   email: '',
   username: '',
   password: '',
+  confirm: '',
 };
 
 const RegisterForm = () => {
@@ -15,32 +17,55 @@ const RegisterForm = () => {
   });
 
   return (
-    <form onSubmit={handleSubmit} className="register-form">
-      <label>E-mail</label>
-      <input
-        type="email"
-        name="email"
-        onChange={handleChange}
-        value={values.email}
-      />
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="register-form">
+        <fieldset>
+          <h1>Foxticket</h1>
+          {/*<label>E-mail</label>*/}
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            placeholder="E-mail..."
+            autoComplete="off"
+          />
 
-      <label>User Name</label>
-      <input
-        type="text"
-        name="username"
-        onChange={handleChange}
-        value={values.username}
-      />
+          {/*<label>User Name</label>*/}
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={values.username}
+            placeholder="User Name..."
+            autoComplete="off"
+          />
 
-      <label>Password</label>
-      <input
-        type="password"
-        name="password"
-        onChange={handleChange}
-        value={values.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+          {/*<label>Password</label>*/}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            placeholder="Password..."
+            autoComplete="off"
+          />
+
+          <input
+            type="password"
+            name="confirm"
+            onChange={handleChange}
+            value={values.confirm}
+            placeholder="Confirm password..."
+            autoComplete="off"
+          />
+
+          <Button type="submit" buttonSize="btn--medium">
+            Sign Up
+          </Button>
+        </fieldset>
+      </form>
+    </div>
   );
 };
 
