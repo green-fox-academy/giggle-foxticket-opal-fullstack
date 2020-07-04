2e34e426bfc36d4c3a5dd198db11ab5ca7b1fb8a
Lehel <l.eli.12@hotmail.com>
2020-07-04 13:57:11 +0200

    help me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,11 @@ import Login from './components/login/login';
 function App() {
   return (
     <div className="App">
-{/*       <LandingPage />
- */}      <Login />
+      <LandingPage />
+      <Login />
     </div>
   );
 }
 
 export default App;
+
