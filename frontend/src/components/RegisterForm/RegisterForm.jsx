9aac2bc239506d5b69e8bd1fa0d8970b387549ca
marcianszlovak <marcianszlovak@gmail.com>
2020-07-04 15:38:32 +0200

    feat(register-form-component) include custom hook

--- frontend/src/components/RegisterForm/RegisterForm.jsx
+++ frontend/src/components/RegisterForm/RegisterForm.jsx
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useCustomForm from '../../hooks/useCustomForm';
+import './RegisterForm.styles.sass';
+
+const initialValues = {
+  email: '',
+  username: '',
+  password: '',
+};
 
 const RegisterForm = () => {
-  const [username, setUserName] = useState('');
-  const handleSubmit = e => e.preventDefault();
+  const { values, handleChange, handleSubmit } = useCustomForm({
+    initialValues,
+    onSubmit: values => console.log({ values }),
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Foxticket</label>
+    <form onSubmit={handleSubmit} className="register-form">
+      <label>E-mail</label>
+      <input
+        type="email"
+        name="email"
+        onChange={handleChange}
+        value={values.email}
+      />
+
+      <label>User Name</label>
       <input
         type="text"
-        value={username}
-        required
-        onChange={e => setUserName(e.target.value)}
+        name="username"
+        onChange={handleChange}
+        value={values.username}
       />
-      <input type="submit" value="add user" />
+
+      <label>Password</label>
+      <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        value={values.password}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };
