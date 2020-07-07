3093c815c4234e5ca912fe0d2ed7452e18867ce5
Tamas Wittek <tammas19@gmail.com>
2020-07-07 20:25:43 +0200

    feat(LandingPage): add Footer component to LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainFeatures from '../components/MainFeatures/MainFeatures';
 import ImageComposition from '../components/ImageComposition/ImageComposition';
+import Footer from '../components/Footer/Footer';
 
 const LandingPage = () => {
   return (
     <>
       <ImageComposition />
       <MainFeatures />
+      <Footer/>
     </>
   );
 };
