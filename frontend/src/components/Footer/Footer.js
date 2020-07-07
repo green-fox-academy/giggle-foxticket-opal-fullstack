d3609d2c0bbf51210ce4e29a1763769ff2d9da13
Tamas Wittek <tammas19@gmail.com>
2020-07-07 20:21:47 +0200

    feat(footer): insert code into Footer component

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Footer.styles.sass';
+import fb from '../../assets/images/logos/facebook.svg';
+import ig from '../../assets/images/logos/instagram.svg';
+import tw from '../../assets/images/logos/twitter.svg';
+
+
+export default function () {
+  return (
+   
+    <footer>
+    
+      <div className="logos">
+        
+        <img src={fb} />
+        <img src={ig} />
+        <img src={tw}/>
+        
+      </div>
+     
+      <h3>I am the Footer</h3>
+    </footer>
+ 
+  )
+}
