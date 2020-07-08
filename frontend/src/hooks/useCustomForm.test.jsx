81545f024fadadfdca6a116ef263bd0237ea42b1
marci <marcianszlovak@gmail.com>
2020-07-08 19:45:34 +0200

    fix(hooks): remove unnecessary file/folder

--- frontend/src/hooks/useCustomForm.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import useCustomForm from './useCustomForm';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<useCustomForm />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
