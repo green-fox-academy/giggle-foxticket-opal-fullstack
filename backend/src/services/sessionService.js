63da3db5d5e2762af590ade6180595bc49a3715d
marci <marcianszlovak@gmail.com>
2020-07-08 20:19:31 +0200

    fix: merge conflicts

--- backend/src/services/sessionService.js
+++ backend/src/services/sessionService.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 // create and read jwt token
-=======
 const jwt = require('jsonwebtoken');
 
 module.exports = {
@@ -10,10 +8,9 @@ module.exports = {
 
   verifyToken: token => {
     try {
-      return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+      return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     } catch (err) {
       res.sendStatus(400).send('Invalid Token');
     }
-  }
+  },
 };
->>>>>>> 975a394f7a482fc57bbdc895127b33c91d029caa
