1892c0373b8fc5e2bdb50de546474f23e8f26078
Tamas Wittek <tammas19@gmail.com>
2020-07-08 16:13:28 +0200

    fix: deleted empty rows

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,19 +7,13 @@ import tw from '../../assets/images/logos/twitter.svg';
 
 export default function () {
   return (
-   
     <footer>
-    
       <div className="logos">
-        
         <img src={fb} alt="Facebook" />
         <img src={ig} alt="Instagram"/>
         <img src={tw} alt="Twitter"/>
-        
       </div>
-     
       <h3>I am the Footer</h3>
     </footer>
- 
   )
 }
