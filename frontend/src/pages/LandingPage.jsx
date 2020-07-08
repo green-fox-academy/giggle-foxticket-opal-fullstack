8f625fcdbe354a363b04a1c33546dbd7d8a06226
marci <marcianszlovak@gmail.com>
2020-07-08 19:23:42 +0200

    fix: conflicts

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import MainFeatures from '../components/MainFeatures/MainFeatures';
 import ImageComposition from '../components/ImageComposition/ImageComposition';
-<<<<<<< HEAD
-import Register from '../components/Register/Register';
-=======
 import Footer from '../components/Footer/Footer';
->>>>>>> e9d65319340e755950de72907de548618263a7c0
 
 const LandingPage = () => {
   return (
     <>
-      <Register />
       <ImageComposition />
       <MainFeatures />
-      <Footer/>
+      <Footer />
     </>
   );
 };
-
 export default LandingPage;
