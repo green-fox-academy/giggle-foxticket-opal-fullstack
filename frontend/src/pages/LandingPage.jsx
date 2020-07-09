b315613d28280788ecb6b29c9d9b1f2f51a705a4
Tamas Wittek <tammas19@gmail.com>
2020-07-09 20:30:13 +0200

    feat: add VideoPlayer component to LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MainFeatures from '../components/MainFeatures/MainFeatures';
 import ImageComposition from '../components/ImageComposition/ImageComposition';
 import Footer from '../components/Footer/Footer';
+import VideoPlayer from '../components/VideoPlayer/VideoPlayer';
 
 const LandingPage = () => {
   return (
     <>
       <ImageComposition />
       <MainFeatures />
+      <VideoPlayer />
       <Footer/>
     </>
   );
