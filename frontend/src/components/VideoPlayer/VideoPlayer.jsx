6aee6ca0ee6239a7921590d43ec83e8915291ab4
Tamas Wittek <tammas19@gmail.com>
2020-07-09 20:22:54 +0200

    add code to VideoPlayer component and create style file

---
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+import './VideoPlayer.styles.sass';
+
+export default function () {
+  return (
+    <div className="video">
+      <ReactPlayer url='https://www.youtube.com/watch?v=qREKP9oijWI' controls={true} />
+    </div>
+  )
+}
