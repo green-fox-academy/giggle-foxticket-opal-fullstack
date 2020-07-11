61e2c20531e931de9a86bd44324d293f81db76c7
Wittek Tamas <56359956+tamas-w@users.noreply.github.com>
2020-07-11 14:48:18 +0200

    GFO-3: add VideoPlayer component with test (#23)
    * feat(VideoPlayer): create VideoPlayer folder and component file inside
    
    * add code to VideoPlayer component and create style file
    
    * feat(VideoPlayer): create test file and insert code
    
    * feat: add react-player into dependencies
    
    * feat: add VideoPlayer component to LandingPage
    
    * feat: add snapshot test for VideoPlayer and update LandingPage snapshot
    
    * fix: add new line at the end of VideoPlayer style file
    
    * fix: add new line at the end of VideoPlayer style file
    
    * fix: update LandingPage snapshot test
    
    * fix: add VideoPlayer to LandingPage, changed video url, update snapshots

---
+++ frontend/src/components/VideoPlayer/VideoPlayer.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import VideoPlayer from './VideoPlayer';
+
+
+it('renders correctly', () => {
+  const tree = renderer.create(<VideoPlayer />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
