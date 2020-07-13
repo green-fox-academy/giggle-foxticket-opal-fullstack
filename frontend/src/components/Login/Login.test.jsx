db6f19ca46b146d3de3eea0e2e757cf05757b289
Szlovák Marcián <56042419+marcianszlovak@users.noreply.github.com>
2020-07-13 10:36:20 +0200

    Remove old backend files, rename Login component, update Login component snapshot test (#27)
    * fix(backend): remove old files
    
    * fix(app): remove unused import
    
    * fix(login-component): rename directory and files, update snapshot test accordingly

--- frontend/src/components/Login/Login.test.jsx
+++ frontend/src/components/Login/Login.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Login from './login';
+import Login from './Login';
 import renderer from 'react-test-renderer';
 
 test('first snapshot of Login', () => {
-  const component = renderer.create(<Login />)
+  const component = renderer.create(<Login />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
-})
+});
