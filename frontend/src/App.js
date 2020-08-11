d54218a8f31883c3bda8aaa082f8d4810ff3b48c
marci <marcianszlovak@gmail.com>
2020-08-11 20:29:45 +0200

    feat(GFO-43): Introduce Redux and redux-thunk on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,27 @@ import AdminTicketTypes from './components/TicketTypes/AdminTicketTypes';
 import Login from './components/Login/Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import { Provider } from 'react-redux';
+import store from './flux/store';
 
 function App() {
+  /* useEffect(() => {
+    store.dispatch(loadUser()); --- for when we have an actual endpoint for it...
+  }, []); */
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <PrivateRoute exact path="/shop" component={TicketStore} />
-          <Route exact path="/admin" component={AdminTicketTypes} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <PrivateRoute exact path="/shop" component={TicketStore} />
+            <Route exact path="/admin" component={AdminTicketTypes} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ frontend/src/flux/actions/errorActions.js
@@ -0,0 +1,14 @@
+import { CLEAR_ERRORS, GET_ERRORS } from './types';
+
+export const returnErrors = (message, status, id = null) => {
+  return {
+    type: GET_ERRORS,
+    payload: { message, status, id },
+  };
+};
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
+  };
+};
