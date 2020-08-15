3e14e410441ef10695a6f3b462917d83b225c6e4
marcianszlovak <marcianszlovak@gmail.com>
2020-08-15 17:54:07 +0200

    feat(GFO-35): Introduce react-modal on the front-end

--- frontend/src/components/ShopTicketList/ShopTicketList.jsx
+++ frontend/src/components/ShopTicketList/ShopTicketList.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './ShopTicketList.styles.sass';
 import Ticket from '../Ticket/Ticket';
 import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
+import useModal from '../../hooks/useModal';
 
 const shopTicketTypes = [
   {
@@ -9,45 +11,46 @@ const shopTicketTypes = [
     title: 'Ticket-Types A',
     description: 'Lorem Ipsum has been the industry s standard dummy',
     iconName: 'FaBeer',
-    buttonText: 'BUY',
-    canEdit: false,
   },
   {
     id: 2,
     title: 'Ticket-Types B',
     description: 'Since the 1500s,when an unknown printer took a galley',
     iconName: 'FaRegSmileWink',
-    buttonText: 'BUY',
-    canEdit: false,
   },
   {
     id: 3,
     title: 'Ticket-Types C',
     description: 'Remaining  essentially unchanged. It was popularised',
     iconName: 'FaRegGem',
-    buttonText: 'BUY',
-    canEdit: false,
   },
   {
     id: 4,
     title: 'Ticket-Types D',
     description: 'Contrary to popular Lorem Ipsum is not  random text',
     iconName: 'FaUmbrella',
-    buttonText: 'BUY',
-    canEdit: false,
   },
 ];
 
 const ShopTicketList = () => {
+  const { isShowing, toggle } = useModal();
+
   return (
     <div className="ticket-list-container">
       <div className="ticket-list">
         <h1 className="main-title">Ticket Types</h1>
         {shopTicketTypes.map(shopTicket => (
           <Ticket key={shopTicket.id} {...shopTicket}>
-            <Button>BUY</Button>
+            <Button onClick={toggle}>BUY</Button>
           </Ticket>
         ))}
+        <Modal hide={toggle} isShowing={isShowing}>
+          <Button onClick={toggle}>OK</Button>
+          <Button buttonStyle={'btn--danger--solid--btn'} onClick={toggle}>
+            Cancel
+          </Button>
+          Are you sure?
+        </Modal>
       </div>
     </div>
   );
