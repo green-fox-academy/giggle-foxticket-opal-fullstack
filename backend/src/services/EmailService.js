04ea2e1cc1c18700ff010a2cc7e1c3de69a3bd0f
Tamas Wittek <tammas19@gmail.com>
2020-08-18 18:15:26 +0200

    fix(GFO-18): fixed msg content in EmailService

--- backend/src/services/EmailService.js
+++ backend/src/services/EmailService.js
@@ -5,10 +5,8 @@ export const EmailService = {
     sgMail.setApiKey(sgMail.setApiKey(process.env.SENDGRID_API_KEY));
     const msg = {
       to: 'test@example.com',
-      from: 'test@example.com',
       subject: 'Sending with Twilio SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      body: 'and easy to do anywhere, even with Node.js'
     };
     try{
       sgMail.send(msg);
