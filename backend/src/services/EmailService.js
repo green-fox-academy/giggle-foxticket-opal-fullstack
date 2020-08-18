4097c3f37c054a6394d102db9b182a35d0eefe6d
Tamas Wittek <tammas19@gmail.com>
2020-08-18 19:40:57 +0200

    feat(GFO-18): changed sending method from sendgrid to mailgun

--- backend/src/services/EmailService.js
+++ backend/src/services/EmailService.js
@@ -1,18 +1,20 @@
-const sgMail = require('@sendgrid/mail');
+let API_KEY = '36253fa6d7e404426e6143fab58a77da-203ef6d0-32d562ff';
+let DOMAIN = 'sandbox074c82faabd845298e075fbdf5cb2a43.mailgun.org';
+let mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
 export const EmailService = {
-  async sendMail() {
-    sgMail.setApiKey(sgMail.setApiKey(process.env.SENDGRID_API_KEY));
-    const msg = {
-      to: 'test@example.com',
-      subject: 'Sending with Twilio SendGrid is Fun',
-      body: 'and easy to do anywhere, even with Node.js'
+  async sendMail(to, subject, body) {
+    const data = {
+      from: 'Excited User <me@samples.mailgun.org>',
+      to,
+      subject,
+      text: body
     };
     try{
-      sgMail.send(msg);
+      mailgun.messages().send(data)
     } catch(error){
        console.log(error)
-    }
-  },
+    } 
+  }
 };
 
