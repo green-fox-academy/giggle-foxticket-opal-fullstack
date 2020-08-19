9bdbdaa591dc1034623125a85321d4258c3cb174
Tamas Wittek <tammas19@gmail.com>
2020-08-19 17:52:39 +0200

    fix(GFO-18): fix mailgun import method and variable type

--- backend/src/services/EmailService.js
+++ backend/src/services/EmailService.js
@@ -1,6 +1,9 @@
-let API_KEY = process.env.API_KEY;
-let DOMAIN = 'sandbox074c82faabd845298e075fbdf5cb2a43.mailgun.org';
-let mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+import mailgunDefault from 'mailgun-js';
+
+const mailgun = mailgunDefault({
+  apiKey: '36253fa6d7e404426e6143fab58a77da-203ef6d0-32d562ff',
+  domain: 'sandbox074c82faabd845298e075fbdf5cb2a43.mailgun.org'
+});
 
 export const EmailService = {
   async sendMail(from, to, subject, body) {
