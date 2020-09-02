b625f90ae0a67c56b4aebad12918cd5f990dd5a3
Tamas Wittek <tammas19@gmail.com>
2020-09-02 20:32:20 +0200

    feat(GFO-139/140): added redux element and imports

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,12 +3,9 @@ import FoxticketLogo from '../../assets/images/logos/Foxticket.png'
 import './Header.styles.sass'
 import { Link } from "react-router-dom";
 import Logout from './Logout';
+import { connect } from 'react-redux';
 
-export default function Header(props) {
-
-// localStorage.removeItem('token');
-// <li className="nav-links" onClick={logoutSession}>Logout</li>
-
+function Header(props) {
 
   return (  
     <nav className="NavbarItems" >
@@ -19,9 +16,17 @@ export default function Header(props) {
           props.type ==='admin' &&
           <Link to="/admin" className="nav-links" >Admin</Link>
         }
-          <li className="nav-links">Username</li>
+        <li className="nav-links">{props.user}</li>
           <Logout />
         </ul>
     </nav>
   )
 }
+
+const mapStateToProps = (state) =>{
+  return {
+    user: state.auth.user
+  }
+}
+
+export default connect (mapStateToProps)(Header)
