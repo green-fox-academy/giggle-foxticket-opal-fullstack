e0ae7d52932220ca4274796746117aff11b5f85d
Tamas Wittek <tammas19@gmail.com>
2020-09-04 21:27:38 +0200

    feat(GFO-139/140): destructed props and added isAdmin

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,26 +7,37 @@ import './Header.styles.sass'
 
 function Header({userName, isAdmin}) {
   return (
-    <nav className="NavbarItems" >
-      <img src={FoxticketLogo} alt='navigation bar logo'></img>
-      <h3 className="nav-text">Foxticket</h3>
-      <ul className="nav-menu">
-      { 
-        isAdmin &&
-         <Link to="/admin" className="nav-links" >Admin</Link>
-      }
-        <li className="nav-links">{userName}</li>
-        <Logout />
-      </ul>
-    </nav>
+    <>
+    { userName &&
+      <nav className="NavbarItems" >
+        <img src={FoxticketLogo} alt='navigation bar logo'></img>
+        <h3 className="nav-text">Foxticket</h3>
+        <ul className="nav-menu">
+        { 
+          isAdmin &&
+          <Link to="/admin" className="nav-links" >Admin</Link>
+        }
+          <li className="nav-links">{userName}</li>
+          <Logout />
+        </ul>
+      </nav>
+    }
+    </>
   )
 }
 
 const mapStateToProps = (state) => {
-  return {
-    userName: state.user.name,
-    isAdmin: state.user.isAdmin
-  }
+    if(state.user){
+      return{
+        userName: state.user.name,
+        isAdmin: state.user.isAdmin
+      }
+    }else{
+      return{
+        userName: null,
+        isAdmin: false
+      }
+   }
 }
 
 export default connect (mapStateToProps)(Header)
