0816e97665c69e8cd63425f4845ddb48540d5e31
Tamas Wittek <tammas19@gmail.com>
2020-09-04 21:11:52 +0200

    feat(GFO-139/140): added Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import { Provider } from 'react-redux';
 import store from './flux/store';
+import Header from './components/Header/Header';
 
 function App() {
   /* useEffect(() => {
@@ -18,6 +19,7 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <div className="App">
+          <Header />
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <PrivateRoute exact path="/shop" component={TicketStore} />
