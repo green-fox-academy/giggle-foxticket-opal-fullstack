ac0905a2247bff412a34f658ede510c8c2d40f39
Tamas Wittek <tammas19@gmail.com>
2020-09-04 21:28:21 +0200

    fix(GFO-139/140): removed Header component

--- frontend/src/pages/TicketStore/TicketStore.jsx
+++ frontend/src/pages/TicketStore/TicketStore.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ShopTicketList from '../../components/ShopTicketList/ShopTicketList';
 import UserTicketList from '../../components/UserTicketList/UserTicketList';
-import Header from '../../components/Header/Header'
 
 const TicketStore = () => {
   return (
     <div className="ticket-store">
-      <Header type="user" />
       <ShopTicketList />
       <UserTicketList />
     </div>
