0b31827822685c15dc5edc4fb2c5c274d91022a0
Tamas Wittek <tammas19@gmail.com>
2020-09-08 20:16:37 +0200

    feat: expanding res.json with user and isAdmin

--- backend/src/controllers/sessionsController.js
+++ backend/src/controllers/sessionsController.js
@@ -6,8 +6,12 @@ export const sessionsController = {
   async post(req, res) {
     const user = req.body;
     try {
-      const token = await sessionService.login(user);
-      res.json({ token }); 
+      const data = await sessionService.login(user);
+      res.json({ 
+        token: data.token,
+        user: data.user,
+        isAdmin: data.isAdmin
+      }); 
     } catch (err) {
       return res.status(401).json({ message: err.message });
     }
