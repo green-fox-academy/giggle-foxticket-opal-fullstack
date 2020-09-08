81ad4e9ca784562a4d8e5298bcf2d332f27940e7
marci <marcianszlovak@gmail.com>
2020-09-08 21:25:42 +0200

    feat(GFO-180): add dependencies for production, edit app.js to serve react front-end

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 import { api, system } from './routes';
 import logger from './logger';
@@ -7,11 +8,17 @@ import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(morgan('combined', { stream: logger.stream }));
-
 app.use('/api', api);
 app.use('/system', system);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+});
+
 
 app.use(errorHandler);
 
 export default app;
+
