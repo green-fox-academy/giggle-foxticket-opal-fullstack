a984a1d86c9b25ae4a367662d14a744c5690fae5
marci <marcianszlovak@gmail.com>
2020-09-19 20:01:20 +0200

    Squashed commit of the following:
    commit 49ea639648e65227b3e441d444b4d3201aaa40bb
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 19:46:55 2020 +0200
    
        edit backend message
    
    commit c766b1862d8d89193c49cfffcb56ea1bc5ae8bcf
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:38:14 2020 +0200
    
        finish fixing header
    
    commit 5d887bc2df12a35406a6ebd75bdbf9b60f2127fc
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:12:54 2020 +0200
    
        fix proptypes
    
    commit 363adf3c722d7531713237bbce667d0826e9fcc1
    Merge: 84ed027 e2554e9
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:03:31 2020 +0200
    
        merge
    
    commit 84ed027234935a4b7a814f96acbd26f61639d504
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 17:08:00 2020 +0200
    
        fix card
    
    commit 79314df6af18fb824e7f63ddd2fc7be0abea352c
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 14:23:39 2020 +0200
    
        fix register
    
    commit 93fa2fc8a366352d3529c9d681ab551d3a4c5187
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 12:48:42 2020 +0200
    
        fix register in app

--- backend/src/middlewares/validators/userValidator.js
+++ backend/src/middlewares/validators/userValidator.js
@@ -28,7 +28,9 @@ export const validateUser = [
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() });
+      return res
+        .status(422)
+        .json({ message: 'There was an error during register...' });
     next();
   },
 ];
