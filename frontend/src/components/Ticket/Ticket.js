1bf1800a95c202e254823806c6692a6d9b80bbd7
Tamas Wittek <tammas19@gmail.com>
2020-09-21 22:26:35 +0200

    fix: restored Ticket component back to it's original version

--- frontend/src/components/Ticket/Ticket.js
+++ frontend/src/components/Ticket/Ticket.js
@@ -2,12 +2,8 @@ import React from 'react';
 import './Ticket.style.sass';
 import GiveIcon from '../DynamicIcon/DynamicIcon';
 import PropTypes from 'prop-types';
-import Modal from '../Modal/Modal';
-import useModal from '../../hooks/useModal';
-import QR from '../QR/QR';
 
-function Ticket({id, title, description, iconName, userTicket, ...props }) {
-  const { isShowing, toggle } = useModal();
+function Ticket({ title, description, iconName, ...props }) {
   return (
     <div className="admin_container">
       <div className="admin_icon">
@@ -17,24 +13,15 @@ function Ticket({id, title, description, iconName, userTicket, ...props }) {
         <h1 className="admin_title">{title}</h1>
         <p className="admin_description">{description}</p>
       </div>
-      <div className="btns" onClick={toggle}>{props.children}</div>
-      { userTicket &&
-        <Modal hide={toggle} isShowing={isShowing}>
-          <div className="TicketModal" onClick={toggle}>
-            <QR id={id}/>
-          </div>
-        </Modal>
-      }
+      <div className="btns">{props.children}</div>
     </div>
   );
 }
 
 Ticket.propTypes = {
-  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   iconName: PropTypes.string.isRequired,
-  userTicket: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
 };
 
