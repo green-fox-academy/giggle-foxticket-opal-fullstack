0c22e12e0c8e2ddc2b195f90d69798775e99ecbe
marci <marcianszlovak@gmail.com>
2020-09-21 16:19:46 +0200

    fix(GFO-185): snapshot test

--- frontend/src/components/Register/Register.test.jsx
+++ frontend/src/components/Register/Register.test.jsx
@@ -3,15 +3,22 @@ import Register from './Register';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import store from '../../flux/store';
+import renderer from 'react-test-renderer';
 
 describe('Register Component', () => {
-  it('render correctly', () => {
-    const wrapper = shallow(
-      <Provider store={store}>
-        <Register />
-      </Provider>
-    );
+  const MockRegister = (
+    <Provider store={store}>
+      <Register />
+    </Provider>
+  );
+
+  it('should render correctly', () => {
+    const wrapper = shallow(MockRegister);
     expect(wrapper.contains(<Register />)).toBe(true);
-    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot', () => {
+    const tree = renderer.create(MockRegister).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
