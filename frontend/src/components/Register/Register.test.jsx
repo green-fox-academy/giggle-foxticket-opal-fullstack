d8bc461ebe7295a60cd532087d254d575b7f28eb
Szlovák Marcián <56042419+marcianszlovak@users.noreply.github.com>
2020-09-22 10:06:22 +0200

    GFO-185: Fix header (#51)
    * feat(GFO-185): include register component in App
    
    * Squashed commit of the following:
    
    commit c766b1862d8d89193c49cfffcb56ea1bc5ae8bcf
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:38:14 2020 +0200
    
        finish fixing header
    
    commit 5d887bc2df12a35406a6ebd75bdbf9b60f2127fc
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:12:54 2020 +0200
    
        fix proptypes
    
    commit 363adf3c722d7531713237bbce667d0826e9fcc1
    Merge: 84ed027 e2554e9
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:03:31 2020 +0200
    
        merge
    
    commit 84ed027234935a4b7a814f96acbd26f61639d504
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 17:08:00 2020 +0200
    
        fix card
    
    commit 79314df6af18fb824e7f63ddd2fc7be0abea352c
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 14:23:39 2020 +0200
    
        fix register
    
    commit 93fa2fc8a366352d3529c9d681ab551d3a4c5187
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 12:48:42 2020 +0200
    
        fix register in app
    
    * add register to header
    
    * Squashed commit of the following:
    
    commit 49ea639648e65227b3e441d444b4d3201aaa40bb
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 19:46:55 2020 +0200
    
        edit backend message
    
    commit c766b1862d8d89193c49cfffcb56ea1bc5ae8bcf
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:38:14 2020 +0200
    
        finish fixing header
    
    commit 5d887bc2df12a35406a6ebd75bdbf9b60f2127fc
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:12:54 2020 +0200
    
        fix proptypes
    
    commit 363adf3c722d7531713237bbce667d0826e9fcc1
    Merge: 84ed027 e2554e9
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 18:03:31 2020 +0200
    
        merge
    
    commit 84ed027234935a4b7a814f96acbd26f61639d504
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 17:08:00 2020 +0200
    
        fix card
    
    commit 79314df6af18fb824e7f63ddd2fc7be0abea352c
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 14:23:39 2020 +0200
    
        fix register
    
    commit 93fa2fc8a366352d3529c9d681ab551d3a4c5187
    Author: marci <marcianszlovak@gmail.com>
    Date:   Sat Sep 19 12:48:42 2020 +0200
    
        fix register in app
    
    * fix(GFO-185): re-add snapshot test for Register Component
    
    * fix(GFO-185): snapshot test

--- frontend/src/components/Register/Register.test.jsx
+++ frontend/src/components/Register/Register.test.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Register from './Register';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../../flux/store';
 import renderer from 'react-test-renderer';
 
-describe('test register component', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Register />).toJSON();
+describe('Register Component', () => {
+  const MockRegister = (
+    <Provider store={store}>
+      <Register />
+    </Provider>
+  );
+
+  it('should render correctly', () => {
+    const wrapper = shallow(MockRegister);
+    expect(wrapper.contains(<Register />)).toBe(true);
+  });
+
+  it('should match the snapshot', () => {
+    const tree = renderer.create(MockRegister).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
